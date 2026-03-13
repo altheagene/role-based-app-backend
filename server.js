48bb4873acc1637571f619dd451746efcc48e6c6
Althea <altheagene@users.noreply.github.com>
2026-03-13 12:10:15 +0800

    Added middleware to parse JSON bodies, log incoming requests, and log errors

--- server.js
+++ server.js
@@ -4,5 +4,31 @@ const jwt = require('jsonwebtoken');
 const cors = require('cors');
 
 const app = express();
-const PORT = 5500;
+const PORT = 3000;
 const SECRET_KEY = 'my$3cretk3yH3H3';
+
+app.use(cors({
+    origin: ['http://127.0.0.1:5500', 'http://localhost:5500']
+}))
+
+//====================== MIDDLEWARE ==========================
+
+//parse JSON
+app.use(express.json());            
+
+//log incoming requests
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+})
+
+//log errors
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Server listening on http://localhost:${PORT}`);
+})
