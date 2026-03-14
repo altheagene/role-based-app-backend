5608493157019f7bef50621ee6bb11d3127adca7
Althea <altheagene@users.noreply.github.com>
2026-03-14 23:04:38 +0800

    updated server.js by adding function to authorizeRole

--- server.js
+++ server.js
@@ -101,3 +101,13 @@ function authenticateToken(req, res, next){
         next();
     })
 }
+
+function authorizeRole(role){
+    return (req, res, next) => {
+        if(req.user.role !== role){
+            return res.status(403).json({error: 'Access denied: insufficient permissions'});
+        }
+
+        next();
+    }
+}
