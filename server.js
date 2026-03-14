219ecd08920837e4934a2e2314810e60b84e133c
Althea <altheagene@users.noreply.github.com>
2026-03-14 22:59:34 +0800

    updated server.js by adding function to authenticate token

--- server.js
+++ server.js
@@ -92,6 +92,12 @@ function authenticateToken(req, res, next){
     const token = authHeader && authHeader.split(' ')[1];
 
     if(!token){
-        
+        return res.status(401).json({error : 'Access token required'});
     }
+
+    jwt.verify(token, SECRET_KEY, (err, user) => {
+        if (err) return res.status(403).json({error: 'Invalid or expired token.'});
+        req.user = user;
+        next();
+    })
 }
